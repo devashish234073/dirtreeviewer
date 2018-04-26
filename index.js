84127dfaecdc1bb8767c09c9fdbc64eb5f80a889
Devashish Priyadarshi <devashish234073@gmail.com>
2018-04-26 23:02:26 +0530

    initial ha ha ha

---
+++ index.js
@@ -0,0 +1,149 @@
+var fs = require("fs");
+
+var ignoreTxt;
+
+function main(){
+    var MAXLEN = process.argv.length;
+    var actionName;
+    var argVal;
+    if(MAXLEN>=3){
+        for(var i=2;i<MAXLEN;i++) {
+            var arg = process.argv[i];
+            if(arg.indexOf("--") > -1) {
+                if(arg == "--help") {
+                    showUsage();
+                } else if(arg == "--fName") {
+                    if(i+1 < MAXLEN) {
+                        if(actionName === undefined) {
+                            actionName = arg;
+                            argVal     = process.argv[i+1];
+                        }
+                    } else {
+                        print("value for '--fName' arg not found.");
+                        showUsage();
+                    }
+                } else if(arg == "--dir") {
+                    if(i+1 < MAXLEN) {
+                        actionName = arg;
+                        argVal     = process.argv[i+1]
+                    } else {
+                        print("value for '--dir' arg not found.");
+                        showUsage();
+                    }
+                }  else if(arg == "--ignr") {
+                    if(i+1 < MAXLEN) {
+                        ignoreTxt = process.argv[i+1];
+                    } else {
+                        print("value for '--ignr' arg not found.");
+                        showUsage();
+                    }
+                }
+            }
+        }
+        doAction(actionName,argVal);
+    } else {
+        showUsage();
+    }
+}
+
+function doAction(actionName,argVal){
+    if(actionName === "--fName") {
+        processFile(argVal);
+    } else if(actionName === "--dir") {
+        var separator = "/";
+        if(argVal.indexOf("\\") > -1){
+            separator = "\\";
+        }
+        if(argVal[argVal.length-1] === separator) {
+            argVal=argVal.substring(0,argVal.length-1);
+        }
+        var dirlist = {};
+        processDir(argVal,dirlist,separator,true);
+        displayAsTree("",dirlist);
+    } else {
+        print("Unable to interpret arguments provided!");
+        showUsage();
+    }
+}
+
+function print(txt) {
+    console.log(txt);
+}
+
+function showUsage() {
+    print("Usage:");
+    print("--help\tShow this usage")
+    print("--fName\tFile with dot separated packages");
+    print("--dir\tDirectory to trace");
+    print("--ignr\tIgnore file/folder containing this text");
+    process.exit(0);
+}
+
+function processFile(fName) {
+    fs.readFile(fName,(err,data)=>{
+        if(err) {
+            print(`Error reading '${fName}'\n${err}`);
+        } else {
+            var lines=String(data).split("\n");
+            var tree=processLines(lines);
+            displayAsTree("",tree);
+        }
+    });
+}
+
+function displayAsTree(spaces,tree){
+    for(key in tree) {
+        if(ignoreTxt !== undefined && key.indexOf(ignoreTxt) > -1) {
+            //ignore
+        } else {
+            print(spaces+">"+key);
+            displayAsTree(spaces+"    ",tree[key]);
+        }
+    }
+}
+
+function processLines(lines) {
+    if(lines.length === 0){
+        print("No content to process!");
+    } else {
+        var root = {};
+        var i    = 0;
+        var j    = 0;
+        var line = "";
+        var arr  = [];
+        var curr = {};
+        for(i=0;i<lines.length;i++){
+            line = lines[i].trim();
+            arr  = line.split(".");
+            curr = root;
+            for(j=0;j<arr.length;j++){
+                if(curr[arr[j]] === undefined){
+                    curr[arr[j]] = {};
+                }
+                curr = curr[arr[j]];
+            }
+        }
+    }
+    return root;
+}
+
+function processDir(dir,curr,separator,isFirst) {
+    var files;
+    var i;
+    try {
+        files = fs.readdirSync(dir);
+        for(i=0;i<files.length;i++){
+            if(curr[files[i]]===undefined){
+                curr[files[i]]={};
+            }
+            newDir = dir+separator+files[i];
+            processDir(newDir,curr[files[i]],separator,false);
+        }
+    } catch(exception){
+        if(isFirst){
+            print(`Unable to read dir '${dir}'\n${exception}`);
+        }
+    }
+}
+
+main();
